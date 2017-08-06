a37c4815badc6805351179b328c5b81554d33251
randomt4sk <Christopher.Peters@stud.uni-due.de>
2017-08-06 13:15:01 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -8,37 +8,40 @@ import xbmc
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
-import simplejson as json
+import requests
+import json
+import urllib
 import AutoCompletion
 
 ADDON = xbmcaddon.Addon()
 ADDON_VERSION = ADDON.getAddonInfo('version')
+PORT = int(ADDON.getSetting('port'))
 
-
-def get_kodi_json(method, params):
-    json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "%s", "params": %s, "id": 1}' % (method, params))
-    json_query = unicode(json_query, 'utf-8', errors='ignore')
-    return json.loads(json_query)
-
+def get_xbmc_json(method, params):
+    headers = {'content-type': 'application/json'}
+    xbmc_host = 'localhost'
+    xbmc_port = PORT
+    xbmc_json_rpc_url = "http://" + xbmc_host + ":" + str(xbmc_port) + "/jsonrpc"
+    payload = {"jsonrpc":"2.0","method":method,"params":params,"id":1}
+    url_param = urllib.urlencode({'request': json.dumps(payload)})
+    response = requests.get(xbmc_json_rpc_url + '?' + url_param, 
+                        headers=headers)
+    return json.loads(response.text)
 
 def start_info_actions(infos, params):
     for info in infos:
         if info == 'autocomplete':
             listitems = AutoCompletion.get_autocomplete_items(params["id"], params.get("limit", 10))
         elif info == 'selectautocomplete':
-            if params.get("handle"):
-                xbmcplugin.setResolvedUrl(handle=int(params.get("handle")),
-                                          succeeded=False,
-                                          listitem=xbmcgui.ListItem())
+            resolve_url(params.get("handle"))
             try:
-                window = xbmcgui.Window(10103)
-            except Exception:
+                window_id = xbmcgui.getCurrentWindowDialogId()
+                window = xbmcgui.Window(window_id)
+            except:
                 return None
             window.setFocusId(300)
-            get_kodi_json(method="Input.SendText",
-                          params='{"text":"%s", "done":false}' % params.get("id"))
+            get_xbmc_json(method="Input.SendText",params={"text":params.get("id"),"done":False})
             return None
-            # xbmc.executebuiltin("SendClick(103,32)")
         pass_list_to_skin(data=listitems,
                           handle=params.get("handle", ""),
                           limit=params.get("limit", 20))
@@ -51,8 +54,9 @@ def pass_list_to_skin(data=[], handle=None, limit=False):
         return None
     if data:
         items = create_listitems(data)
+        items = [(i.getProperty("path"), i, bool(i.getProperty("directory"))) for i in items]
         xbmcplugin.addDirectoryItems(handle=handle,
-                                     items=[(i.getProperty("path"), i, False) for i in items],
+                                     items=items,
                                      totalItems=len(items))
     xbmcplugin.endOfDirectory(handle)
 
@@ -62,7 +66,7 @@ def create_listitems(data=None):
         return []
     itemlist = []
     for (count, result) in enumerate(data):
-        listitem = xbmcgui.ListItem(str(count))
+        listitem = xbmcgui.ListItem('%s' % (str(count)))
         for (key, value) in result.iteritems():
             if not value:
                 continue
@@ -74,29 +78,45 @@ def create_listitems(data=None):
                 listitem.setPath(path=path)
                 listitem.setProperty('path', path)
         listitem.setProperty("index", str(count))
-        listitem.setProperty("isPlayable", "false")
         itemlist.append(listitem)
     return itemlist
 
 
-if (__name__ == "__main__"):
-    xbmc.log("version %s started" % ADDON_VERSION)
-    args = sys.argv[2][1:]
-    handle = int(sys.argv[1])
-    infos = []
-    params = {"handle": handle}
-    delimiter = "&&"
-    for arg in args.split(delimiter):
-        param = arg.replace('"', '').replace("'", " ")
-        if param.startswith('info='):
-            infos.append(param[5:])
+def resolve_url(handle):
+    if handle:
+        xbmcplugin.setResolvedUrl(handle=int(handle),
+                                  succeeded=False,
+                                  listitem=xbmcgui.ListItem())
+
+
+class Main:
+
+    def __init__(self):
+        xbmc.log("version %s started" % ADDON_VERSION)
+        self._parse_argv()
+        if self.infos:
+            start_info_actions(self.infos, self.params)
+
+    def _parse_argv(self):
+        args = sys.argv[2][1:]
+        self.handle = int(sys.argv[1])
+        self.infos = []
+        self.params = {"handle": self.handle}
+        if args.startswith("---"):
+            delimiter = "&"
+            args = args[3:]
         else:
-            try:
-                params[param.split("=")[0].lower()] = "=".join(param.split("=")[1:]).strip()
-            except Exception:
-                pass
-    if infos:
-        start_info_actions(infos, params)
+            delimiter = "&&"
+        for arg in args.split(delimiter):
+            param = arg.replace('"', '').replace("'", " ")
+            if param.startswith('info='):
+                self.infos.append(param[5:])
+            else:
+                try:
+                    self.params[param.split("=")[0].lower()] = "=".join(param.split("=")[1:]).strip()
+                except:
+                    pass
 
+if (__name__ == "__main__"):
+    Main()
 xbmc.log('finished')
-
